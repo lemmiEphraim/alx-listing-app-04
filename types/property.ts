fe1572f79ac0f4dbe690df18fe4f34938983d3e1
lemmiEphraim <lemmiabdissa@gmail.com>
2025-09-03 00:40:18 +0300

    Update property.ts

--- types/property.ts
+++ types/property.ts
@@ -1,1 +1,15 @@
-
+export interface Property {
+  id: number;
+  title: string;
+  location: string;
+  price: number;
+  rating: number;
+  reviewCount: number;
+  image: string;
+  type: string;
+  bedrooms?: number;
+  bathrooms?: number;
+  area?: number;
+  description?: string;
+  amenities?: string[];
+}
